a41a5ae497d0a8de04e067784d4462a548687465
estebancabreraa <esteban.cabrera.a@gmail.com>
2018-02-14 08:54:58 -0600

    Terminado.

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+
+    public float time = 0;
+    public bool active = true;
+    public GameObject child;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if ((time >= 2))
+        {
+            active = !active;
+            child.SetActive(active);
+            time = 0;
+            
+        }
+	}
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -12,19 +12,22 @@ public class Character : MonoBehaviour {
     private float speed = 5f;
     private float jumpForce = 250f;
     private bool facingRight = true;
- 
+    public GameObject feet;
+    public LayerMask layerMask;
 
 
-	void Start () {
+    void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
-	
 
-	void Update () {
+
+    void Update()
+    {
         float move = Input.GetAxis("Horizontal");
-        if (move != 0) {
+        if (move != 0)
+        {
             rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
             facingRight = move > 0;
         }
@@ -33,8 +36,15 @@ public class Character : MonoBehaviour {
 
         sr.flipX = !facingRight;
 
-        if (Input.GetButtonDown("Jump")) {
-            rb2d.AddForce(Vector2.up*jumpForce);
+        if (Input.GetButtonDown("Jump"))
+        {
+            RaycastHit2D raycast =
+            Physics2D.Raycast(feet.transform.position, Vector2.down, 0.1f,
+            layerMask);
+            if (raycast.collider != null)
+            {
+                rb2d.AddForce(Vector2.up * jumpForce);
+            }
         }
-	}
+    }
 }
